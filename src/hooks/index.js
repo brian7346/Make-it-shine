1cc2673fc09e8d60e21bbedd01c103ed81f26c69
maxroslow <brian2009@yandex.ru>
2019-08-29 16:11:18 +0400

    useProjects hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -48,3 +48,28 @@ export const useTasks = selectedProject => {
 
   return { tasks, archivedTasks };
 };
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('projects')
+      .where('userId', '==', 'jlIFXIwyAL3tzHMtzRbw')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
+          ...project.data(),
+          docId: project.id
+        }));
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
+
+  return { projects, setProjects };
+};
