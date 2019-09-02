93635864872e3627d9e61064ad34139f303b6920
maxroslow <brian2009@yandex.ru>
2019-09-02 17:04:39 +0400

    feat: Project overlay

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,8 @@ import { FaRegListAlt, FaRegCalendarAlt } from 'react-icons/fa';
 import moment from 'moment';
 import { firebase } from '../firebase';
 import { useSelectedProjectValue } from '../context';
+import { ProjectOverlay } from './ProjectOverlay';
+import { TaskDate } from './TaskDate';
 
 export const AddTask = ({
   showAddTaskMain = true,
@@ -89,8 +91,12 @@ export const AddTask = ({
               </div>
             </>
           )}
-          <p>Project overlay here</p>
-          <p>TaskDate here</p>
+          <ProjectOverlay
+            setProject={setProject}
+            showProjectOverlay={showProjectOverlay}
+            setShowProjectOverlay={setShowProjectOverlay}
+          />
+          <TaskDate />
           <input
             className="add-task__content"
             data-testid="add-task-content"
@@ -103,7 +109,7 @@ export const AddTask = ({
             type="button"
             className="add-task__submit"
             data-testid="add-task"
-            onChange={() => addTask()}
+            onClick={() => addTask()}
           >
             Add Task
           </button>
---
+++ src/components/ProjectOverlay.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useProjectsValue } from '../context';
+
+export const ProjectOverlay = ({
+  setProject,
+  showProjectOverlay,
+  setShowProjectOverlay
+}) => {
+  const { projects } = useProjectsValue();
+  return (
+    projects &&
+    showProjectOverlay && (
+      <div className="project-overlay" data-testid="project-overlay">
+        <ul className="project-overlay__list">
+          {projects.map(project => (
+            <li
+              key={project.projectId}
+              data-testid="project-overlay__action"
+              onClick={() => {
+                setProject(project.projectId);
+                setShowProjectOverlay(false);
+              }}
+            >
+              {project.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
+};
