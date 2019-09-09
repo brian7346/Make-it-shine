f856b8c6a5820206feff14b32d348ae40d9d1899
Max Roslow <doctorbrian97@gmail.com>
2019-09-09 17:22:15 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: 'AIzaSyBTJEkFnIBos-QYcG3_mxFfd4-5xNDXp40',
-  authDomain: 'todolist-97d95.firebaseapp.com',
-  databaseURL: 'https://todolist-97d95.firebaseio.com',
-  projectId: 'todolist-97d95',
-  storageBucket: 'todolist-97d95.appspot.com',
-  messagingSenderId: '276911635533',
-  appId: '1:276911635533:web:8608b8ffdfee448b'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 });
 
 export { firebaseConfig as firebase };
